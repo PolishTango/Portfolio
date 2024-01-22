b80c885730a3671d664b11632dd101494a43a193
Daniel Fronczak <Daniel Fronczak>
2024-01-22 18:13:36 +0100

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage } from './Main/HomePage';
 import Main from './Main/Main';
 import Projects from './Main/Projects';
